746fdee49a822a3309a3cdc3b45041f298dbc81a
Jeremy Sfez <jeremy@smooth-code.com>
2022-08-02 12:09:59 +0200

    feat: add gatsby-plugin-screenshots

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,20 @@ module.exports = {
     title: `argos-gatsby-github-actions`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // test local package
   // plugins: [`argos-javascript/packages/gatsby-plugin-screenshot`],
   plugins: [
-    ...(process.env.CI
-      ? [
-          {
-            resolve: "gatsby-plugin-argos",
-            options: {
-              branch: process.env.GITHUB_REF_NAME,
-              commit: process.env.GITHUB_SHA,
-              token: process.env.ARGOS_TOKEN,
-            },
-          },
-        ]
-      : []),
+    {
+      resolve: `gatsby-plugin-screenshot`,
+      options: { dir: "./screenshots" },
+    },
+    {
+      resolve: `gatsby-plugin-argos`,
+      options: {
+        branch: process.env.GITHUB_REF_NAME,
+        commit: process.env.GITHUB_SHA,
+        token: process.env.ARGOS_TOKEN,
+        dir: "screenshots",
+      },
+    },
   ],
 };
