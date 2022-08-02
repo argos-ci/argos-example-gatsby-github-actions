41948fb1d603d7e83f733d6b03f0344ed004ff7f
Jeremy Sfez <jeremy@smooth-code.com>
2022-08-02 11:49:28 +0200

    feat: add gatsby-plugin-argos

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,13 @@ module.exports = {
     title: `argos-gatsby-github-actions`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  // test local package
+  // plugins: [`argos-javascript/packages/gatsby-plugin-screenshot`],
   plugins: [
     ...(process.env.CI
       ? [
           {
-            resolve: 'gatsby-plugin-argos',
+            resolve: "gatsby-plugin-argos",
             options: {
               branch: process.env.GITHUB_REF_NAME,
               commit: process.env.GITHUB_SHA,
@@ -17,4 +19,4 @@ module.exports = {
         ]
       : []),
   ],
-}
+};
